37844f63654233b84ad25d2dcededa31c7a22578
dongzhao <41535479+Naoketong@users.noreply.github.com>
2020-03-08 20:56:11 +0800

    order_details

--- pages/order_details/order_details.js
+++ pages/order_details/order_details.js
@@ -5,6 +5,7 @@ Page({
         car_id: '',
         order: [],
         cost: [],
+        order_state: '',
 
     },
     onLoad: function(options) {
@@ -15,12 +16,33 @@ Page({
     getOrder: function(e) {
         let id = this.data.order_number;
         indexService.orderFind(id).then(order => {
+            console.log(order[0].order_state)
             this.setData({ order })
+            this.setData({ order_state: order[0].order_state })
             let id = order[0].car_id;
             indexService.costItem(id).then(cost => {
-                console.log(cost)
+                // console.log(cost)
                 this.setData({ cost })
             })
+        })
+    },
+    orderCancel: function() {
+        let id = this.data.order_number;
+
+        let order_state = 3;
+        console.log(id, order_state)
+        indexService.ordermMdify(id, { order_state }).then(order => {
+            console.log(order)
+            wx.showToast({
+                title: '取消成功',
+            });
+
+            setTimeout(() => {
+                wx.switchTab({
+                    url: '/pages/index/index'
+                })
+            }, 500);
+
         })
     },
 
